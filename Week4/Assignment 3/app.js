1ec0f8112b967ce2f8fdb08277e6665d2d6cc0a3
Jonathan Lo <jonathanlo411@gmail.com>
2023-04-20 20:06:07 -0700

    q1 partal q2

--- Week4/Assignment 3/app.js
+++ Week4/Assignment 3/app.js
@@ -2,7 +2,7 @@ function assignment3(){
     //Use this function to call solution functions of other questions 
     let names = ['Amy', 'Jake', 'Sara', 'Liam', 'Robert', 'Harry', 'Liam', 'Jake', 'Sara', 'Taylor', 'Kathy',                'Amy', 'Jake', 'Harry', 'Nina', 'Robert', 'Harry']
     question0()
-    question1()
+    question1(5)
     question2()
     question3()
     question4()
@@ -15,12 +15,32 @@ question1=function(n){
     //This function prints the pattern mentioned in Question 1
     //You should not hard code the pattern in arrays.
     //This pattern should be generated using JS code (for loops and if-else statements)
-    
+    const target = document.getElementById("q1");
+    let starCount = 1;
+    let buffer = n - 1;
+    for (let i = 0; i < n; i ++) {
+        target.innerHTML += `<pre>${" ".repeat(buffer)}${"*".repeat(starCount)}</pre>`
+        starCount += 2;
+        buffer--;
+    }
 }
 
 question2=function(){
-// Print the {Name:Count} data structure to the console and the names which occur more than 2 times in index.html.
-       
+    // Print the {Name:Count} data structure to the console and the names which occur more than 2 times in index.html.
+    let temp32 = ['Amy', 'Jake', 'Sara', 'Liam', 'Robert', 'Harry', 'Liam', 'Jake', 'Sara', 'Taylor', 'Kathy',                'Amy', 'Jake', 'Harry', 'Nina', 'Robert', 'Harry']
+    const target = document.getElementById("q2")
+
+    // p1
+    let counts = new Object;
+    for (let name of temp32) {
+        if (name in counts) { counts[name]++ } else { counts[name] = 1 }
+    }
+    console.log(counts)
+
+    // p2
+    for (let name of Object.keys(counts)) {
+        (counts[name] > 2) ? target.innerHTML += name + ", " : undefined
+    }
 }
 
 
